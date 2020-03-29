3e481c1c6e41620564512e0cde10468f4c0dbf40
Rebecca N <natterer.re@gmail.com>
2020-03-29 19:27:44 +0000

    modified projects listings

--- include/loop/description.php
+++ include/loop/description.php
@@ -2,6 +2,8 @@
 defined( 'ABSPATH' ) || exit;
 
 $col_num = get_option('number_of_words_show_in_listing_description', 130);
+$terms = get_the_terms(get_the_id(), 'product_cat');
 ?>
 
-<p class="wpneo-short-description"><?php echo wpcf_function()->limit_word_text(strip_tags(get_the_content()), $col_num); ?></p>
+  <p class="wpneo-short-description"><?php echo wpcf_function()->limit_word_text(strip_tags(get_the_content()), $col_num); ?></p>
+
--- include/loop/thumbnail.php
+++ include/loop/thumbnail.php
@@ -1,13 +1,87 @@
 <?php
 defined( 'ABSPATH' ) || exit;
 ?>
+<style type="text/css">
+  .thumbnail-overlay-top {
+    width: 100%;
+    height: 50px;
+    background: var(--main-color);
+    display: block;
+  }
+  
+  .thumbnail-overlay-top h5 {
+    padding: 0;
+    padding-left: 15px;
+  }
+  
+  .thumbnail-overlay-top h6 {
+    color: #fff;
+    padding: 0;
+    padding-left: 15px;
+    font-size: 12px;
+  }
+  
+  .thumbnail-overlay-top h5 a:link,
+  .thumbnail-overlay-top h5 a:visited,
+  .thumbnail-overlay-top h6 span,
+  .thumbnail-overlay-top h6 i,
+  .thumbnail-overlay-bottom h5 span,
+  .thumbnail-overlay-bottom h5 i{
+    color: #fff;
+  }
+  
+  .thumbnail-overlay-bottom {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 50px;
+    background: rgba(0,0,0,0.7);
+    display: block;
+    border: 0;
+    border-top: 4px solid var(--main-color);
+    color: #fff;
+  }
+  
+  .thumbnail-overlay-bottom h5 {
+    color: #fff;
+    padding: 0;
+    padding-left: 15px;
+    padding-top: 10px;
+    font-size: 13px;
+    width: 90%;
+  }
+ 
+  .thumbnail-overlay-bottom h5 a:link,
+  .thumbnail-overlay-bottom h5 a:visited {
+    color: #fff;
+    float: right;
+    font-size: 25px;
+  }
+</style>
+
+<div class="thumbnail-overlay-top">
+  <h5>
+    <a href="<?php echo get_permalink(); ?>"><?php  echo get_the_title(); ?></a>
+  </h5>
+  <h6>
+    <i class="fa fa-map-marker"></i>
+    <span><?php echo wpcf_function()->campaign_location(); ?></span>
+  </h6>
+</div>
+
 <div class="wpneo-listing-img">
-    <a href="<?php echo get_permalink(); ?>" title="<?php  echo get_the_title(); ?>"> <?php echo woocommerce_get_product_thumbnail(); ?></a>
-    <div class="overlay">
-		<div>
-			<div>
-				<a href="<?php echo get_permalink(); ?>"><?php _e('View Campaign','wp-crowdfunding'); ?></a>
-			</div>
-		</div>
-	</div>
+  <a href="<?php echo get_permalink(); ?>" title="<?php  echo get_the_title(); ?>"> <?php echo woocommerce_get_product_thumbnail(); ?></a>
+  
+  <div class="thumbnail-overlay-bottom">
+    <h5>
+      <i class="fa fa-users"></i>
+      <span>
+        <?php
+        $terms = get_the_terms(get_the_id(), 'product_cat');
+        ?>
+        <?php _e($terms[0]->name, 'wp-crowdfunding'); ?></span>
+      <a href="<?php echo get_permalink(); ?>"><i class="fa fa-comments"></i></a>
+      <div style="clear: both;"></div>
+    </h5>
+  </div>
 </div>
